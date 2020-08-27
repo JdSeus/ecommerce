9acc4756617dbc669522a3d902a8fab0a15bfb02
Juliano Seus <juliano.seus@hotmail.com>
2020-08-27 17:21:28 -0300

    Esqueceu a Senha - Site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -177,7 +177,7 @@ class User extends Model {
 		));
 	}
 
-	public static function getForgot($email)
+	public static function getForgot($email, $inadmin = true)
 	{
 
 		$sql = new Sql();
@@ -224,7 +224,12 @@ class User extends Model {
 
 				//$code = base64_encode(openssl_encrypt($dataRecovery["idrecovery"], "AES-128-CBC", User::SECRET));
 
-				$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+				if ($inadmin === true) {
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+				} else {
+
+					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+				}
 
 				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
 					"name"=>$data["desperson"],
