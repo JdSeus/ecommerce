8c94b26aa994ef22f0a1be0bf8adff1e1409daf1
Juliano Seus <juliano.seus@hotmail.com>
2020-09-01 14:51:56 -0300

    Admin - Pedidos

--- site.php
+++ site.php
@@ -504,7 +504,8 @@ $app->get("/boleto/:idorder", function($idorder){
 	$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
 	$valor_cobrado = $order->getvltotal(); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
 	$valor_cobrado = str_replace(",", ".",$valor_cobrado);
-	$valor_boleto = number_format($valor_cobrado + $taxa_boleto, 2, ',', '');
+	$valor_cobrado = $valor_cobrado + $taxa_boleto;
+	$valor_boleto = number_format($valor_cobrado, 2, ',', '');
 
 	$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
 	$dadosboleto["numero_documento"] = $order->getidorder();;	// Num do pedido ou nosso numero
