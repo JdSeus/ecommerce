89d4b793d10d68174bbd81a35b496efe048fc571
Juliano Seus <juliano.seus@hotmail.com>
2020-09-02 14:39:33 -0300

    Admin - Categorias - Busca e PAginacao

--- admin-categories.php
+++ admin-categories.php
@@ -8,15 +8,42 @@ use \Hcode\Page;
 
 $app->get("/admin/categories", function(){
 
-		User::verifyLogin();
+	User::verifyLogin();
 
-		$categories = Category::listAll();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-		$page = new PageAdmin();
+	if ($search != '') {
 
-		$page->setTpl("categories", [
-		'categories'=>$categories
+		$pagination = Category::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Category::getPage($page);
+
+	}
+
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+		array_push($pages, [
+			'href'=>'/admin/categories?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
 		]);
+	}
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+	"categories"=>$pagination['data'],
+	"search"=>$search,
+	"pages"=>$pages
+	]);
 
 });
 
